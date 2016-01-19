d3df17d66db17e22285a6b7b834cecdf0951763c
olivergoodman <ollygreatman@gmail.com>
2016-01-19 13:30:30 -0600

    first commit
    looking for way to update database on each page hit by retrieving data
    thru JQuery

--- my_app/hello/models.py
+++ my_app/hello/models.py
@@ -1,5 +1,23 @@
+from my_app import db
 
-MESSAGES = {
-       'default': 'Hello!',
-       'test': 'this is a test message', #http://127.0.0.1:5000/show/test
-}
+class Page(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255))
+	page_hits = db.Column(db.Integer, default=0)
+
+	def __init__(self, name): #initalize new page to 0 page_hits
+		self.name = name
+		self.page_hits = 0
+
+	def __repr__(self):
+		return '<Page %d>' % self.name
+
+	def visitPage(self):
+		self.page_hits = self.page_hits + 1 
+ 
+#create pages and add them to the database --???
+home = Page('home')
+contact = Page('contact')
+db.session.add(home)
+db.session.add(contact)
+db.session.commit()
--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template, request
-from my_app.hello.models import MESSAGES
+
 
 hello = Blueprint('hello', __name__)
+
+
+def updatePageDB():
+	#retrieve data from JS somehow
+	title = 'home'
+	current_page = Page.query.filter_by(username=title).first()
+	current_page.visitPage()
+
 
 @hello.route('/')
 @hello.route('/index.html')
@@ -19,11 +27,3 @@ def contact():
 	title = 'Contact',
 	page_title = page_title)
 
-@hello.route('/show/<key>')
-def get_message(key):
-    return MESSAGES.get(key) or "%s not found!" % key
-
-@hello.route('/add/<key>/<message>')
-def add_or_update_message(key, message):
-    MESSAGES[key] = message
-    return "%s Added/Updated" % key 
