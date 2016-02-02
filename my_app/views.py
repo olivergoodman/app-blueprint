cc8ae112d0f3374fb429952c23e2791e8f8442b9
olivergoodman <ollygreatman@gmail.com>
2016-02-02 12:22:59 -0600

    began new database
    started implementing new database by having backend take JSON
    parameters from script that found page title.

--- my_app/views.py
+++ my_app/views.py
@@ -8,6 +8,12 @@ hello = Blueprint('hello', __name__)
 #create new route taking JSON parameter to determine what to log in the database
 #pull database branch onto master, then delete old one and replace it with new one!
 
+@hello.route('/_get_page_title')
+def get_page_title():
+	page_title = request.args.get('page_title', '', type=string)
+	app.updatePageDB(page_title)
+	#update DB depending on what JSON received ---> http://flask.pocoo.org/docs/0.10/patterns/jquery/
+
 @hello.route('/')
 @hello.route('/index.html')
 def index():
